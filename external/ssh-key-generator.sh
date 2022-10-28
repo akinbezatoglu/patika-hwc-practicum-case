3bf724a10329ad0e2ae8c18e54883c8b72778ad4
akinbezatoglu <akinbezatoglu@gmail.com>
2022-10-28 23:11:46 +0300

    Add ssh key generator for remote login to instances

---
+++ external/ssh-key-generator.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# generate ssh key pair file name
+# with 8 digits random characters
+fname="KeyPair-$(
+    cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 8
+    echo
+)"
+
+# Check if the .ssh file is in the directory
+# and create the .ssh file if it isn't.
+if [[ ! -d "./external/.ssh" ]]; then
+    mkdir ./external/.ssh
+fi
+
+# file path
+fpath="./external/.ssh/${fname}"
+
+# check if such a file exists in the directory
+# and generate an ssh key file.
+if [[ ! -f "${fpath}" ]]; then
+    ssh-keygen -q -t rsa -N '' -b 2048 -v -f $fpath -m PEM
+fi
+
+# add .pem extension to the file
+mv ${fpath} "${fpath}.pem"
+
+# export tf env variables for keypair resource
+export TF_VAR_publickey=$(cat ${fpath}.pub)
+export TF_VAR_keyname=${fname}
