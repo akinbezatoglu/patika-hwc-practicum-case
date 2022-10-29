ea9bcefd994d86636341e6037a349d8dc687444e
akinbezatoglu <akinbezatoglu@gmail.com>
2022-10-29 14:18:08 +0300

    Add databese password

--- external/key-mock.sh
+++ external/key-mock.sh
@@ -3,3 +3,6 @@
 # Set secrets via environment variables
 export TF_VAR_auth_accesskey="your-access-key-id"
 export TF_VAR_auth_secretkey="your-secret-key-id"
+
+# Set mysql db password in rds instance
+export TF_VAR_mysql_psswd="your-password"
