c053648ec64c3179433289fa45a62661d5c8ce4c
akinbezatoglu <akinbezatoglu@gmail.com>
2022-10-30 19:17:50 +0300

    Delete external.tf

--- external.tf
+++
@@ -1,7 +0,0 @@
-data "external" "auth" {
-  program = ["bash", "${path.module}/external/key.sh"]
-}
-
-data "external" "ssh" {
-  program = ["bash", "${path.module}/external/ssh-key-generator.sh"]
-}
