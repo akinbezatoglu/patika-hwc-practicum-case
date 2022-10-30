f950c6f523bd8e88dfd56e1f46b3efbad724d6f9
akinbezatoglu <akinbezatoglu@gmail.com>
2022-10-30 18:28:07 +0300

    Add all variables in tf root module

--- variable.tf
+++ variable.tf
@@ -7,7 +7,6 @@ variable "availability_zones" {
   default = ["ap-southeast-1a", "ap-southeast-1b"]
 }
 
-# External variables
 variable "auth_accesskey" {
   description = "The access key id of the user's account"
   type        = string
@@ -20,14 +19,133 @@ variable "auth_secretkey" {
   sensitive   = true
 }
 
-variable "publickey" {
+variable "secg_name" {
+  type    = string
+  default = "secg-allow-all"
+}
+
+variable "secg_ingress_direct" {
+  type    = string
+  default = "ingress"
+}
+
+variable "ethertype" {
+  type    = string
+  default = "IPv4"
+}
+
+variable "all_ip_addr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "vpc1" {
+  type        = map(string)
+  description = "(optional) describe your variable"
+  default = {
+    name              = "vpc-001",
+    cidr_block        = "10.123.0.0/22"
+    subnet            = "subnet-001"
+    subnet_cidr_block = "10.123.0.0/24"
+    subnet_gateway_ip = "10.123.0.1"
+  }
+}
+
+variable "vpc2" {
+  type        = map(string)
+  description = "(optional) describe your variable"
+  default = {
+    name              = "vpc-002",
+    cidr_block        = "10.10.12.0/22"
+    subnet            = "subnet-002"
+    subnet_cidr_block = "10.10.12.0/24"
+    subnet_gateway_ip = "10.10.12.1"
+  }
+}
+
+variable "vpc3" {
+  type        = map(string)
+  description = "(optional) describe your variable"
+  default = {
+    name              = "vpc-003",
+    cidr_block        = "10.57.240.0/22"
+    subnet            = "subnet-003"
+    subnet_cidr_block = "10.57.240.0/24"
+    subnet_gateway_ip = "10.57.240.1"
+  }
+}
+
+variable "dns_list" {
+  type    = list(string)
+  default = ["100.125.1.250", "100.125.129.250"]
+}
+
+variable "ecs1_name" {
+  type        = string
+  description = "(optional) describe your variable"
+  default     = "ecs-001"
+}
+
+variable "ecs2_name" {
+  type        = string
+  description = "(optional) describe your variable"
+  default     = "ecs-002"
+}
+
+variable "ecs_flavor" {
+  default = "s3.large.2"
+}
+
+variable "eip_bandwidth" {
+  type        = map(string)
+  description = "(optional) describe your variable"
+  default = {
+    publicip_type = "5_bgp"
+    name          = "bw"
+    size          = 8
+    share_type    = "PER"
+    charge_mode   = "traffic"
+  }
+}
+
+variable "public_key" {
   description = "The public key content of generated ssh key pair file"
   type        = string
   sensitive   = true
 }
 
-variable "keyname" {
+variable "keypair_name" {
   description = "The name of generated ssh key pair file"
   type        = string
   sensitive   = true
+}
+
+variable "ubuntu_img" {
+  type    = string
+  default = "Ubuntu 18.04 server 64bit"
+}
+
+variable "rds1_name" {
+  type    = string
+  default = "rds-001"
+}
+
+variable "rds_flavor" {
+  default = "rds.mysql.n1.large.2.ha"
+}
+
+variable "ha_replication_mode" {
+  default = "semisync"
+}
+
+variable "db" {
+  type        = map(string)
+  description = "() describe your variable"
+  default = {
+    type        = "MySQL"
+    version     = "5.7"
+    password    = "akin@1234"
+    volume_type = "CLOUDSSD"
+    volume_size = "40"
+  }
 }
